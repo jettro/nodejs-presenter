b1556f7f5701e9109bb56ec2029d1e4427fc1c86
jettro <jettro@gridshore.nl>
2011-06-05 14:27:53 +0200

    started with the change to abstract the presentation and make it possible to show multiple presentations

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var pub = __dirname + '/public';
-
 var Slide = require('./slide');
+var Presentation = require('./presentation');
+
 var express = require('express')
-        , app = express.createServer()
-        , slide = new Slide();
+        , app = express.createServer();
+
+var presentations = [];
+var createPresentationNodejs = [];
+createPresentationNodejs.push("intro","Creating a presentation with Node.js");
+createPresentationNodejs.push("goal","Goal");
+presentations.push(new Presentation("Creating a presentation with Node.js","create-presentation-nodejs",createPresentationNodejs,
+    "With this presentation I want to show a way to create a presentation without the usual tools. We are going to create our own tool during the presentation."));
 
 app.configure(function() {
     app.set('view engine', 'jade');
@@ -15,12 +22,23 @@ app.configure(function() {
 });
 
 app.get('/', function(req, res) {
-    res.render('index', {locals: {slides:slide.allSlides()}});
+    res.render('index', {locals: {presentations:presentations}});
 });
-app.get('/slide/:id', slide.index);
-app.post('/slide/:id', slide.command);
+//app.get('/slide/:id', presentation.index);
+//app.post('/slide/:id', slide.command);
 
 app.listen(8018);
 console.log('Express server started on port %s', app.address().port);
 
+//function Slide() {
 
+//    slides.push("Creating a presentation with Node.js",
+//            "Goal",
+//            "Requirements",
+//            "Technology",
+//            "Configure url handling",
+//            "Handle requests",
+//            "View content",
+//            "The Future",
+//            "Questions");
+//}
